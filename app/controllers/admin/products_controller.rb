a02dca858cca9b76de7621ace4778c6374a2b371
Courtney Amos <c.amos@shaw.ca>
2021-05-27 12:16:17 -0700

    implement user authentication

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,7 +1,5 @@
 class Admin::ProductsController < ApplicationController
-  http_basic_authenticate_with  name: ENV['ADMIN_USERNAME'], 
-                                password: ENV['ADMIN_PASSWORD'], 
-                                if: -> { ENV['ADMIN_PASSWORD'].present? }
+  before_filter :authorize
 
   def index
     @products = Product.order(id: :desc).all
--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,14 @@ Rails.application.routes.draw do
 
   resources :orders, only: [:create, :show]
 
+  # routes for showing users a login form, logging then in && out
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+  
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
