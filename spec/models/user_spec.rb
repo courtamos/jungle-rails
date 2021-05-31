38d111c940e9f24d6a3eb949c2e3a3d297ff252a
Courtney Amos <c.amos@shaw.ca>
2021-05-31 15:03:45 -0700

    implement 'validates that password isnt empty' for user rspec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -16,5 +16,14 @@ RSpec.describe User, type: :model do
       expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
     end
 
+    it "validates that password isn't empty" do
+      @user.password = nil
+      @user.password_confirmation = nil
+
+      @user.save
+
+      expect(@user.errors.full_messages).to include("Password can't be blank")
+    end
+
   end
 end
