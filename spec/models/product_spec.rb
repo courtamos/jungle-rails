2cbe041688246d5a491829e5663e31a95fdd6785
Courtney Amos <c.amos@shaw.ca>
2021-05-31 14:06:27 -0700

    implement 'successfully saves' for products rspec

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -4,6 +4,16 @@ RSpec.describe Product, type: :model do
   describe 'Validations' do
 
     it "successfully saves" do
+      @product = Product.new
+
+      @product.name = 'Product Name'
+      @product.price = 100
+      @product.quantity = 1
+      @product.category_id = 1
+
+      @product.save
+
+      expect(@product).to be_present
     end
 
     it "validates name" do
