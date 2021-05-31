cb061a4aba29a4b59c24633abe751268f9877ba2
Courtney Amos <c.amos@shaw.ca>
2021-05-31 14:05:47 -0700

    create rspec for product validations - outline

---
+++ spec/models/product_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe Product, type: :model do
+  describe 'Validations' do
+
+    it "successfully saves" do
+    end
+
+    it "validates name" do
+    end
+
+    it "validates price" do
+    end
+
+    it "validates quantity" do
+    end
+
+    it "validates category" do
+    end
+
+  end
+end
