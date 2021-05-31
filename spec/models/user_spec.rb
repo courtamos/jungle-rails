1bc1f72562f6095ad5f631d51411a79a7f4fdd48
Courtney Amos <c.amos@shaw.ca>
2021-05-31 15:00:54 -0700

    implement 'validates password' for user rspec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "Validations" do
+
+    before(:each) do
+      @user = User.new
+    end
+
+    it "validates password" do
+      @user.password = 'password'
+      @user.password_confirmation = 'notpassword'
+
+      @user.save
+
+      expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
+    end
+
+  end
 end
