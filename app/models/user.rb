72d180925b747b8f04c538c3a5ecdddd6e238c87
Courtney Amos <c.amos@shaw.ca>
2021-05-31 15:48:05 -0700

    implement 'validates that emails are unique' for user rspec and update user model to validate

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,23 @@
 class User < ActiveRecord::Base
 
   has_secure_password
+
+  validates :first_name, :last_name, :password_confirmation, presence: true
+  validates :email, uniqueness: true, presence: true
+  validates :password, presence: true, length: { minimum: 8 }
+
+
+  before_save :cleanup_email
+
+  def cleanup_email
+    self.email.downcase!
+    self.email.strip!
+  end
+
+  def self.authenticate_with_credentials(email, password)
+    email.strip!
+    email.downcase!
+    User.find_by(email: "#{email}").try(:authenticate, "#{password}")
+  end
   
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,13 +3,28 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   describe "Validations" do
 
-    before(:each) do
-      @user = User.new
+    it "validates without errors when valid" do
+      @user = User.new(
+        first_name: 'Prince',
+        last_name: 'Vegeta',
+        email: 'vegeta@email.com',
+        password: 'password',
+        password_confirmation: 'password'
+      )
+
+      @user.save
+
+      expect(@user.errors.full_messages).to eq([])
     end
 
     it "validates password" do
-      @user.password = 'password'
-      @user.password_confirmation = 'notpassword'
+      @user = User.new(
+        first_name: 'Prince',
+        last_name: 'Vegeta',
+        email: 'vegeta@email.com',
+        password: 'password',
+        password_confirmation: 'notpassword'
+      )
 
       @user.save
 
@@ -17,13 +32,41 @@ RSpec.describe User, type: :model do
     end
 
     it "validates that password isn't empty" do
-      @user.password = nil
-      @user.password_confirmation = nil
+      @user = User.new(
+        first_name: 'Prince',
+        last_name: 'Vegeta',
+        email: 'vegeta@email.com',
+        password: nil,
+        password_confirmation: nil
+      )
 
       @user.save
 
       expect(@user.errors.full_messages).to include("Password can't be blank")
     end
+
+    it "validates that emails are unique" do
+      @user = User.new(
+        first_name: 'Prince',
+        last_name: 'Vegeta',
+        email: 'vegeta@email.com',
+        password: 'password',
+        password_confirmation: 'password'
+      )
+      @user.save
+
+      @usercopy = User.new(
+        first_name: 'Prince',
+        last_name: 'Vegeta',
+        email: 'vegeta@email.com',
+        password: 'password',
+        password_confirmation: 'password'
+      )
+      @usercopy.valid?
+
+      expect(@usercopy.errors.full_messages).to include("Email has already been taken")
+    end
+
 
   end
 end
